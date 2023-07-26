d716059b34d040dfe161f35bbe8854b77b75faef
vishalshetty104 <vishalshetty104@gmail.com>
2023-07-26 15:36:12 +0530

    added base files

---
+++ src/main/java/vishalshetty104/gamePriceTracker/GamePriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package vishalshetty104.gamePriceTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePriceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vishalshetty104/gamePriceTracker/GamePriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package vishalshetty104.gamePriceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePriceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
